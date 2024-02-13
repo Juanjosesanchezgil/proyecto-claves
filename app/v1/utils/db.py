0919b56bb4e6a17a6535b0f98c9efba40465fe63
Juanj Sanchez <juanjosesanchezgil@gmail.com>
2024-02-13 21:29:34 +0100

    db

--- app/v1/utils/db.py
+++ app/v1/utils/db.py
@@ -12,3 +12,10 @@ DB_PASS = settings.db_pass
 DB_HOST = settings.db_host
 DB_PORT = settings.db_port
 
+db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None }
+db_state = ContextVar("db_state", default=db_state_default.copy())
+
+class PeeweeConectionState(peewee._ConnectionState):
+    def __init__(self, **kwargs):
+        super().__setattr__("_state", db_state)
+        super().__init__(**kwargs)
